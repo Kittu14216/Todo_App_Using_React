3399de92b82823cb60c29d7d47eea6db949cd61e
Kittu14216 <kittumarisetti@gmail.com>
2024-07-05 00:50:58 +0530

    Todo App Using React

---
+++ todoapp/src/App.jsx
@@ -0,0 +1,18 @@
+// import reactLogo from "./assets/react.svg";
+// import viteLogo from "/vite.svg";
+import Todo from "./components/todo";
+import "./App.css";
+import Header from "./components/Header";
+
+function App() {
+  return (
+    <>
+      <div>
+        <Header />
+        <Todo />
+      </div>
+    </>
+  );
+}
+
+export default App;
